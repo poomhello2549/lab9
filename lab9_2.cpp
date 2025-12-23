487ec6e14c6dae4b411e91c85d84e245d974e1d1
poomhello2549 <poomhello2549@gmail.com>
2025-12-23 13:07:11 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,22 +3,28 @@ using namespace std;
 
 //Write the function printO() here
 
+
+void printO(int x, int y) {
+
+	if(x <= 0 || y < 0){
+
+		cout << "Invalid input" ;
+	}
+
+	for ( int i = 1; i <= x; i++){
+
+		for( int j = 1; j <= y; j++){
+			cout << "O" ;
+		}
+		cout << endl ;
+	}
+}
+
+
 int main(){
 	
-	printO(2,2);
+	printO(10,2);
 	cout << "\n";
 	
-	printO(3,5);
-	cout << "\n";	
 	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
 }
